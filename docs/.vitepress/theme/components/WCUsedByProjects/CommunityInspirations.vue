4e619f224f214d0858193c270b6478e220c8d8ba
donmckenna <don@donmckenna.dev>
2023-02-24 16:37:52 -0700

    chore: update template to handle new data structure

--- docs/.vitepress/theme/components/WCUsedByProjects/CommunityInspirations.vue
+++ docs/.vitepress/theme/components/WCUsedByProjects/CommunityInspirations.vue
@@ -1,63 +1,71 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { communityProjects } from '../../../../data';
-import Card from '@theme/components/CardLists/Card.vue';
 // const { isDark } = useData();
 // defineProps<{ feature: FeaturesCardData }>();
+import { onMounted } from 'vue';
+import { communityProjectCardData } from '../../../../data';
+import Card from '@theme/components/CardLists/Card.vue';
 onMounted(() => {
-  console.log('communityProjects: ', communityProjects);
+  console.log('communityProjectCardData: ', communityProjectCardData);
 })
 </script>
 
 <template>
   <div class="community-projects">
-    
-    <div v-for="project of communityProjects" class="project-item">
+    <a v-for="project of communityProjectCardData" class="project-item" href="#">
       <Card>
         <div class="item-content">
-          <template v-for="(description, i) in project.descriptionItems">
+          <img :src="project.thumbnailUrl" />
 
-            <div v-if="i === 0" class="item-description">
-              <img v-if="description.thumbnailUrl" :src="description.thumbnailUrl" />
-              <div class="description-content">
-                <div class="description-heading">
-                  <span class="project-title">{{ project.title }}</span>
-                  <span class="project-category">{{ project.category }}</span>
-                </div>
-                <p v-html="description.content" />
-              </div>
+          <div class="item-description">
+            <div class="description-heading">
+              <span class="project-title">{{ project.title }}</span>
+              <span class="project-category">{{ project.category }}</span>
             </div>
+            <p v-html="project.description" />
+          </div>
 
-            <a v-else-if="description.itemUrl" class="item-description secondary" :href="description.itemUrl">
-              <img v-if="description.thumbnailUrl" :src="description.thumbnailUrl" />
-              <span v-html="description.content" />
-            </a>
-
-            <div v-else class="item-description secondary">
-              <img v-if="description.thumbnailUrl" :src="description.thumbnailUrl" />
-              <p v-html="description.content" />
-            </div>
-
-          </template>        
         </div>
       </Card>
-    </div>
-
+    </a>
   </div>
 </template>
 
 <style scoped lang="scss">
 .community-projects {
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 24px;
+}
+
+.card {
+  backdrop-filter: none;
+  :root.dark & {
+    backdrop-filter: none;
+  }
 }
 
 .project-item {
+  align-self: flex-start;
+  color: var(--vp-c-text-1);
   border-radius: 16px;
   overflow: hidden;
+  transition: 0.14s ease;
+  transition-property: background-color, box-shadow, color, transform;
+  cursor: pointer;
   :root:not(.dark) & {
-    background-color: rgba(236, 236, 244, 0.32);
+    &:not(:hover) {
+      background-color: rgba(236, 236, 244, 0.32);
+    }
+  }
+  &:hover {
+    color: #000;
+    box-shadow: 0 14px 36px -24px rgba(0,40,100,0.24);
+    background-color: rgba(236, 236, 244, 0.2);
+    transform: translateY(-3px);
+    :root.dark & {
+      color: #fff;
+      background-color: rgba(185, 222, 255, 0.06);
+    }
   }
 }
 
@@ -65,56 +73,21 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   width: 100%;
-  // padding: 12px;
-}
-
-.item-description {
-  display: flex;
-  gap: 8px;
+  padding: 8px;
   img {
-    width: 252px;
-    margin: 12px;
-    border-radius: 6px;
+    margin: 0;
+    border-radius: 8px;
     :root:not(.dark) & {
       box-shadow: 0 1px 2px 0 rgba(196, 208, 217, 0.6);
     }
   }
-  &.secondary {
-    box-shadow:  0 -1px 0 0 rgba(196, 208, 217, 0.4);
-    :root.dark & {
-      box-shadow: 0 -1px 0 0 rgba(193, 228, 255, 0.1);
-    }
-    img {
-      max-width: 92px;
-    }
-  }
 }
 
-a.item-description.secondary {
-  align-items: center;
-  gap: 20px;
-  padding: 8px 12px;
-  color: var(--vp-c-text-2);
-  font-size: 13px !important;
-  line-height: 1.6;
-  transition: 0.1s ease;
-  transition-property: background-color, color;
-  &:hover {
-    color: var(--vp-c-text-1);
-    background-color: rgba(236, 240, 244, 0.4);
-    :root.dark & {
-      background-color: rgba(185, 222, 255, 0.02);
-    }
-  }
-}
-
-.description-content {
-  flex: 1;
+.item-description {
   display: flex;
   flex-direction: column;
   gap: 14px;
-  padding: 28px 24px;
-
+  padding: 20px 20px 16px;
   p,
   a {
     margin: 0;
