e79f991ef4a06c5c870f630c73966a40a5b58ee8
donmckenna <don@donmckenna.dev>
2023-03-14 17:03:36 -0600

    chore: update feedback component styles

--- docs/.vitepress/theme/components/Feedback/Feedback.vue
+++ docs/.vitepress/theme/components/Feedback/Feedback.vue
@@ -15,16 +15,23 @@ enum FeedbackState {
 const route = useRoute();
 const currentState = ref(FeedbackState.START);
 const feedback = ref('');
+const feedbackTextarea = ref(null);
 const isSending = ref(false);
 const hasError = ref(false);
 
 function onButtonClick(value: FeedbackState.YES | FeedbackState.NO) {
+  // don't change state if opposite button is active.
+  const opposingState = value === FeedbackState.YES ? FeedbackState.NO : FeedbackState.YES;
+  if ([opposingState].includes(currentState.value)) {
+    return;
+  }
   currentState.value = value;
-  sendEvent({
-    eventName: 'wc_doc_feedback_helpful',
-    pagePath: route.path,
-    value,
-  });
+  setTimeout(() => document.getElementById('feedback')?.focus());
+  // setTimeout(() => this.$refs.feedbackTextarea.focus());
+}
+
+function cancelForm() {
+  currentState.value = FeedbackState.START;
 }
 
 function encode(data: Record<string, string>) {
@@ -36,6 +43,11 @@ function encode(data: Record<string, string>) {
 // We send a form to Netlify: https://docs.netlify.com/forms/setup/
 function submitForm() {
   isSending.value = true;
+  sendEvent({
+    eventName: 'wc_doc_feedback_helpful',
+    pagePath: route.path,
+    value: currentState.value,
+  });
   fetch('/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -79,26 +91,37 @@ watch(
         <input name="page" />
       </form>
 
-      <template v-if="currentState === FeedbackState.START">
         <p class="title">Was this page helpful?</p>
         <div class="buttonContainer">
 
-          <CardSingle link>
-            <button class="button" @click="onButtonClick(FeedbackState.YES)">
+          <CardSingle :link="![FeedbackState.NO].includes(currentState)">
+            <button class="button"
+              :class="
+                [FeedbackState.YES].includes(currentState) && 'active',
+                [FeedbackState.NO].includes(currentState) && 'deselected'
+              "
+              @click="onButtonClick(FeedbackState.YES)"
+            >
               <span class="icon thumbsUp"></span> Yes
             </button>
           </CardSingle>
 
-          <CardSingle link>
-            <button class="button" @click="onButtonClick(FeedbackState.NO)">
-              <span class="icon thumbsDown"></span>No
+          <CardSingle :link="![FeedbackState.YES].includes(currentState)">
+            <button class="button"
+              :class="
+                [FeedbackState.NO].includes(currentState) && 'active',
+                [FeedbackState.YES].includes(currentState) && 'deselected'
+              "
+              @click="onButtonClick(FeedbackState.NO)"
+            >
+              <span class="icon thumbsDown"></span> No
             </button>
           </CardSingle>
 
+          <button v-if="[FeedbackState.YES, FeedbackState.NO].includes(currentState)" class="cancel" @click="cancelForm">Cancel</button>
         </div>
-      </template>
 
-      <template v-else-if="[FeedbackState.YES, FeedbackState.NO].includes(currentState)">
+      <template v-if="[FeedbackState.YES, FeedbackState.NO].includes(currentState)">
         <form
           class="form"
           name="doc-feedback"
@@ -108,17 +131,18 @@ watch(
         >
           <input type="hidden" name="form-name" value="doc-feedback" />
           <label class="title label" for="feedback">
-            <template v-if="currentState === FeedbackState.YES"> What was most helpful? </template>
-            <template v-else> What should we improve? </template>
+            <template v-if="currentState === FeedbackState.YES"><span class="icon thumbsUp"></span> What did you find most helpful? </template>
+            <template v-else><span class="icon thumbsDown"></span> What should we improve? </template>
           </label>
           <textarea
             id="feedback"
             name="feedback"
             class="textarea"
             v-model="feedback"
+            ref="feedbackTextarea"
             autofocus
           ></textarea>
-          <button class="button" type="submit" :disabled="feedback.length === 0">
+          <button class="button submit" type="submit" :disabled="feedback.length === 0">
             <LoadingIcon v-if="isSending" :size="11" />Submit
           </button>
           <p v-if="hasError" class="error">Sorry, something went wrong. Please try it again later.</p>
@@ -126,7 +150,7 @@ watch(
       </template>
 
       <template v-else-if="currentState === FeedbackState.END">
-        <p class="end">Thank you for helping improve our documentation! &#9829;</p>
+        <p class="end">Thank you for helping improve our documentation! <span class="red">&#9829;</span></p>
       </template>
 
     </div>
@@ -134,6 +158,8 @@ watch(
 
 <style scoped lang="scss">
 .feedbackContainer {
+  --icon-color-green: rgb(0, 185, 163);
+  --icon-color-red: rgb(246, 96, 151);
   margin-block-start: 100px;
   margin-block-end: -8px;
   padding-block-start: 20px;
@@ -148,18 +174,31 @@ watch(
 
 .buttonContainer {
   display: flex;
-  gap: 4px;
+  gap: 6px;
 }
 
 .title {
   color: var(--vp-c-text-2);
-  font-weight: 600;
+  font-weight: 500;
   font-size: 13px;
+}
+.cancel {
+  padding: 0 16px;
+  color: var(--vp-c-text-2);
+  font-size: 12px;
+  font-weight: 500;
+  text-decoration: underline;
+  &:hover {
+    color: var(--vp-c-text-1);
+  }
 }
 
 .label {
-  display: block;
+  display: flex;
+  align-items: center;
+  gap: 12px;
   margin-block-end: 10px;
+  color: var(--vp-c-text-1);
 }
 
 .button {
@@ -175,13 +214,28 @@ watch(
     opacity: 0.4;
     cursor: not-allowed;
   }
-  &:hover {
+
+  &.active,
+  &:hover:not(.deselected):not(.submit:disabled) {
     color: var(--vp-c-text-1);
     background-color: rgba(255,255,255,0.5);
   }
+  &.active {
+    cursor: default;
+  }
 
-  :root.dark &:hover {
+  :root.dark &.active,
+  :root.dark &:hover:not(.deselected):not(.submit:disabled) {
     background-color: var(--vp-c-bg-soft);
+  }
+
+  &.deselected {
+    opacity: 0.4;
+    cursor: default;
+    background-color: transparent;
+    &:hover {
+      background-color: transparent;
+    }
   }
 }
 
@@ -196,16 +250,20 @@ watch(
   -webkit-mask: url('/icons/fa-thumbs-up.svg') center/contain no-repeat;
   mask: url('/icons/fa-thumbs-up.svg') center/contain no-repeat;
 }
-.button:hover .thumbsUp {
-  background-color: rgb(0, 185, 163);
+.title.label .thumbsUp,
+.button.active .thumbsUp,
+.button:hover:not(.deselected) .thumbsUp {
+  background-color: var(--icon-color-green);
 }
 
 .thumbsDown {
   -webkit-mask: url('/icons/fa-thumbs-down.svg') center/contain no-repeat;
   mask: url('/icons/fa-thumbs-down.svg') center/contain no-repeat;
 }
-.button:hover .thumbsDown {
-  background-color: rgb(246, 96, 151);
+.title.label .thumbsDown,
+.button.active .thumbsDown,
+.button:hover:not(.deselected) .thumbsDown {
+  background-color: var(--icon-color-red);
 }
 
 .form {
@@ -216,15 +274,24 @@ watch(
   width: 100%;
   height: 80px;
   padding: 6px 10px;
-  margin-block-end: 10px;
+  margin-block-end: 6px;
   border: 1px solid var(--vp-c-gray-dark-2);
   border-radius: 6px;
   background-color: var(--vp-c-bg);
   font-family: inherit;
   &:focus-visible {
-    outline: solid 2px var(--sb-foreground-highlight);
-    outline-offset: -2px;
+    outline: solid 1px var(--sb-foreground-highlight);
+    outline-offset: -1px;
   }
+}
+
+.button.submit {
+  margin-block-end: 24px;
+  padding: 8px 32px;
+  font-size: 12.5px;
+  font-weight: 600;
+  border-radius: 24px;
+  box-shadow: inset  0 0 0 1px var(--vp-c-bg-soft);
 }
 
 .error {
@@ -235,5 +302,17 @@ watch(
 .end {
   flex: 1;
   text-align: center;
+  color: var(--vp-c-text-1);
+  font-size: 13px;
+  line-height: 1.5;
+  font-weight: 500;
+  padding: 2px 12px;
+  .red {
+    color: var(--icon-color-red);
+  }
+  @media screen and (max-width: 600px) {
+    min-width: 200px;
+    font-size: 11.5px;
+  }
 }
 </style>
