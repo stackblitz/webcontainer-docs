11a2c987e27b1b5671a1a0b5c67d99a2c75ff662
SamVerschueren <sam.verschueren@gmail.com>
2023-04-27 09:28:02 +0200

    chore(safari): enable terminal on Safari 16.4

--- docs/.vitepress/theme/components/Examples/WCEmbed/utils.ts
+++ docs/.vitepress/theme/components/Examples/WCEmbed/utils.ts
@@ -1,13 +1,23 @@
 /**
  * Checks if WebContainer is supported on the current browser.
  */
-export function isWebContainerSupported() {
+export function isWebContainerSupported() {  
   const hasSharedArrayBuffer = 'SharedArrayBuffer' in window;
   const looksLikeChrome = navigator.userAgent.toLowerCase().includes('chrome');
   const looksLikeFirefox = navigator.userAgent.includes('Firefox');
+  const looksLikeSafari = navigator.userAgent.includes('Safari');
 
   if (hasSharedArrayBuffer && (looksLikeChrome || looksLikeFirefox)) {
     return true;
+  }
+
+  if (hasSharedArrayBuffer && looksLikeSafari) {
+    // we only support Safari 16.4 and up so we check for the version here
+    const match = navigator.userAgent.match(/Version\/(\d+)\.(\d+) (?:Mobile\/.*?)?Safari/);
+    const majorVersion = match ? Number(match?.[1]) : 0;
+    const minorVersion = match ? Number(match?.[2]) : 0;
+
+    return majorVersion > 16 || (majorVersion === 16 && minorVersion >= 4);
   }
 
   // Allow overriding the support check with localStorage.webcontainer_any_ua = 1
