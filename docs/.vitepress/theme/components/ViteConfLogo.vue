67cf494adc165f87918f3f35aced27a56938dbf6
Yuka Masuda <yuka1110m@gmail.com>
2023-09-12 23:24:05 +0200

    Add ViteConf top banner (#39)

---
+++ docs/.vitepress/theme/components/ViteConfLogo.vue
@@ -0,0 +1,42 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" fill="none">
+    <g clip-path="url(#a)">
+      <path
+        fill="url(#b)"
+        d="m16.86.01-7.6 1.54c-.08 0-.15.07-.15.07-.08.08-.08.15-.08.15L8.6 10.1v.14c0 .08.07.08.07.08s.07.07.14.07h.15l2.12-.51c.22-.08.37.14.37.36l-.66 3.3c-.07.21.15.43.36.36l1.32-.44c.22-.07.37.15.37.37l-1.03 5.11c-.07.3.37.52.51.22l.15-.22L18.7 5.86c0-.14-.22-.43-.44-.36l-2.2.44c-.22.07-.36-.15-.29-.37l1.4-5.19c.06-.22-.08-.44-.3-.37Z"
+      />
+      <path
+        fill="url(#c)"
+        d="M24.54 2.28c-.3-.44-.88-.66-1.4-.58l-5.77 1.1-.22.87 6.22-1.1c.22-.07.44.08.58.22.15.22.15.44 0 .66l-10.9 19.38c-.07.22-.29.3-.5.3a.62.62 0 0 1-.52-.3L.92 3.38a.44.44 0 0 1 0-.59c.14-.14.29-.22.5-.22h.16l6.58 1.17.07-.8L1.65 1.7a1.4 1.4 0 0 0-1.4.58C-.02 2.72-.1 3.31.2 3.82L11.3 23.27c.22.44.73.73 1.24.73.52 0 .96-.3 1.25-.73l10.9-19.45c.21-.51.21-1.1-.15-1.54Z"
+      />
+    </g>
+    <defs>
+      <linearGradient
+        id="b"
+        x1="8.77"
+        x2="17.34"
+        y1="2.46"
+        y2="13.29"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#B047FF" />
+        <stop offset=".75" stop-color="#FFD0D0" />
+        <stop offset=".92" stop-color="#FFF3E6" />
+      </linearGradient>
+      <linearGradient
+        id="c"
+        x1="6.69"
+        x2="19.14"
+        y1=".27"
+        y2="14.15"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#57CCFF" />
+        <stop offset="1" stop-color="#AF48FF" />
+      </linearGradient>
+      <clipPath id="a">
+        <path fill="#fff" d="M0 0h24.83v24H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
