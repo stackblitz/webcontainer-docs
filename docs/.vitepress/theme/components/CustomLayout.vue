c2649b271019f8b027ae318e32513be2228f3bdf
donmckenna <don@donmckenna.dev>
2023-10-14 00:29:43 -0600

    chore: remove viteconf banner

--- docs/.vitepress/theme/components/CustomLayout.vue
+++ docs/.vitepress/theme/components/CustomLayout.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme';
 import Feedback from './Feedback/Feedback.vue';
-import ViteConfBanner from './ViteConfBanner.vue';
 
 const { Layout } = DefaultTheme;
 </script>
 
 <template>
   <Layout>
-   <template #layout-top>
+    <!-- <template #layout-top>
       <ViteConfBanner />
-    </template>
+    </template> -->
     <template #doc-footer-before>
       <Feedback />
     </template>
