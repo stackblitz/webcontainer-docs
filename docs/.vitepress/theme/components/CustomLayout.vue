0cfe1a5e5c1c0fedb74f5ad048e3f1f37819e3bf
Yuka Masuda <yuka1110m@gmail.com>
2023-10-19 10:19:57 +0200

    Revert "Add ViteConf top banner (#39)"
    This reverts commit 67cf494adc165f87918f3f35aced27a56938dbf6.

--- docs/.vitepress/theme/components/CustomLayout.vue
+++ docs/.vitepress/theme/components/CustomLayout.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme';
 import Feedback from './Feedback/Feedback.vue';
-import ViteConfBanner from './ViteConfBanner.vue';
-
 const { Layout } = DefaultTheme;
 </script>
 
 <template>
   <Layout>
-   <template #layout-top>
-      <ViteConfBanner />
-    </template>
     <template #doc-footer-before>
       <Feedback />
     </template>
