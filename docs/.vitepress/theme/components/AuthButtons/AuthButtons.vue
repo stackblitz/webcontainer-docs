a6dc179a16120c917cd3c6087ec96a89b8d91bfb
ykmsd <yuka1110m@gmail.com>
2024-01-24 10:15:44 +0100

    Add auth buttons

---
+++ docs/.vitepress/theme/components/AuthButtons/AuthButtons.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+defineProps<{ place: 'navScreen' | 'navBar' }>();
+</script>
+
+<template>
+  <div :class="place === 'navScreen' ? 'navScreenContainer' : 'navBarContainer'">
+    <a href="https://stackblitz.com/sign_in" class="link light">Sign in</a>
+    <a href="https://stackblitz.com/register" class="link accent">Get started</a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import './linkStyles.scss';
+
+/**
+ * When we have enough space, we show auth buttons in nav bar.
+ * When we don't, we show them inside the hamburger menu.
+ */
+.navBarContainer {
+  margin-inline-start: 40px;
+  display: flex;
+  gap: 8px;
+  // this is when we hide top nav menu
+  @media screen and (max-width: 768px) {
+    margin-inline-start: 0px;
+  }
+  @media screen and (max-width: 400px) {
+    display: none;
+  }
+
+  .link {
+    @media screen and (max-width: 500px) {
+      height: 30px;
+      padding: 0 10px;
+    }
+  }
+}
+
+.navScreenContainer {
+  display: none;
+  @media screen and (max-width: 400px) {
+    display: flex;
+    gap: 8px;
+    margin-block-start: 24px;
+  }
+
+  .link {
+    flex: 1;
+    justify-content: center;
+    height: 40px;
+  }
+}
+</style>
--- docs/.vitepress/theme/components/CustomLayout.vue
+++ docs/.vitepress/theme/components/CustomLayout.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme';
 import Feedback from './Feedback/Feedback.vue';
+import NavBarAuthButtons from './AuthButtons/NavBarAuthButtons.vue';
+import NavScreenAuthButtons from './AuthButtons/NavScreenAuthButtons.vue';
 
 const { Layout } = DefaultTheme;
 </script>
 
 <template>
   <Layout>
-
+    <template #nav-bar-content-after>
+      <NavBarAuthButtons />
+    </template>
     <!-- <template #layout-top>
       <ViteConfBanner />
     </template> -->
 
     <template #doc-footer-before>
       <Feedback />
+    </template>
+    <template #nav-screen-content-after>
+      <NavScreenAuthButtons />
     </template>
   </Layout>
 </template>
